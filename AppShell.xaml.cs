6a904288352e60891b23c0dd406ccda86f190aef
Martin Honnen <martin.honnen@gmx.de>
2023-02-05 15:38:40 +0100

    Projektdateien hinzufügen.

---
+++ AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace SaxonHEMauiNET7Test1;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
---
+++ MainPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using net.sf.saxon.s9api;
+
+namespace SaxonHEMauiNET7Test1;
+
+public partial class MainPage : ContentPage
+{
+
+    private static readonly Processor processor = new Processor(true);
+
+	public MainPage()
+	{
+		InitializeComponent();
+        HWLabel.Text += $" Saxon {processor.getSaxonEdition()} {processor.getSaxonProductVersion()}";
+
+    }
+
+    private void SaxonDateTest_Clicked(object sender, EventArgs e)
+    {
+
+    }
+}
+
